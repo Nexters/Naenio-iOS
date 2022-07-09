9cc2bca4ac92f9118261f909703e9a00b2bbce29
enebin <lyb35548@gmail.com>
2022-07-09 20:08:07 +0900

    Initial Commit

---
+++ Naenio/NaenioApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaenioApp.swift
+//  Naenio
+//
+//  Created by 이영빈 on 2022/07/09.
+//
+
+import SwiftUI
+
+@main
+struct NaenioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
