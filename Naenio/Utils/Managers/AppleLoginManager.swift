06eda84f06ad256ee46b4b09129ada5f1f3c0e4a
YoungBin Lee <lyb35548@gmail.com>
2022-07-13 08:17:04 +0900

    Set foundations for Apple login (#1)
    * Add Sign-in with apple entitlements
    * Add implementation for Apple login

---
+++ Naenio/Utils/Managers/AppleLoginManager.swift
@@ -0,0 +1,40 @@
+//
+//  AppleLoginManager.swift
+//  Naenio
+//
+//  Created by enebin on 2022/07/10.
+//
+
+import Foundation
+import AuthenticationServices
+
+class AppleLoginManager: NSObject {
+    func requestLoginToServer(with result: ASAuthorization) -> Result<UserInformation, Error> {
+        do {
+            guard let info = result.credential as? ASAuthorizationAppleIDCredential,
+                  let token = info.identityToken,
+                  let stringToken = String(data: token, encoding: .utf8)
+            else {
+                // TODO: Replace here
+                throw URLError(.cannotDecodeRawData)
+            }
+            
+            // TODO: LoginRequestInformation May contain authorization codes
+            let loginInfo = LoginRequestInformation(accessToken: stringToken)
+            let userInfo = try submitUserInformationToServer(with: loginInfo)
+            
+            return .success(userInfo)
+        }
+        catch let error {
+            return .failure(error)
+        }
+        
+    }
+    
+    private func submitUserInformationToServer(with info: LoginRequestInformation) throws -> UserInformation {
+        // let userInfo = API.request(with: info)
+        let mockInfo = UserInformation(name: "", id: "")
+        return mockInfo
+    }
+}
+
---
+++ Naenio/Utils/Models/LoginRequestInformation.swift
@@ -0,0 +1,12 @@
+//
+//  LoginResponse.swift
+//  Naenio
+//
+//  Created by 프라이빗 on 2022/07/10.
+//
+
+import Foundation
+
+struct LoginRequestInformation: Encodable {
+    let accessToken: String
+}
