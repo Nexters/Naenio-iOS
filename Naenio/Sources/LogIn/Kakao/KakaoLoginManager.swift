726c75e55b6271ac343324c1b544deb9322ab07a
enebin <lyb35548@gmail.com>
2022-07-20 21:32:55 +0900

    Squashed commit of the following:
    commit 3f52fef19173b81d2c3b115707730c11f2f24413
    Author: enebin <enebin777@gmail.com>
    Date:   Fri Jul 15 14:30:26 2022 +0900
    
        Add implementations for Kakao login
    
    commit ce0879ee9b81b6c7ed37fb616555a580b7288cba
    Author: enebin <enebin777@gmail.com>
    Date:   Fri Jul 15 14:30:20 2022 +0900
    
        Add comments
    
    commit 7c42cb07441ab5885dbf9ff5b7874ed89829248e
    Author: enebin <enebin777@gmail.com>
    Date:   Fri Jul 15 14:06:47 2022 +0900
    
        Change file structure
    
    commit a85b2de42ed284de448f615601c6304a2a1cc1d3
    Author: enebin <enebin777@gmail.com>
    Date:   Fri Jul 15 14:04:28 2022 +0900
    
        Remove unused codes
    
    commit 3b0a1bfca2255eb4cb39d1eb7a62a3ec2ef134f6
    Author: enebin <enebin777@gmail.com>
    Date:   Fri Jul 15 13:53:46 2022 +0900
    
        Add Kakao login server communication
    
    commit 6b069fe96a7bf285513768c724604afef331e05a
    Author: enebin <enebin777@gmail.com>
    Date:   Fri Jul 15 12:28:41 2022 +0900
    
        Add login button
    
    commit 71121dca04f00c8e5daa3ea0a127caf245733c02
    Author: enebin <enebin777@gmail.com>
    Date:   Thu Jul 14 18:52:11 2022 +0900
    
        Add more dependency
    
    commit 87d67d496a13d31c3a7623e2f82a353b12327146
    Author: enebin <enebin777@gmail.com>
    Date:   Thu Jul 14 18:40:10 2022 +0900
    
        Integrate kakao login to existing model
    
    commit 8396a3f1eb7a6a57f5c80677548434bd78010662
    Author: enebin <enebin777@gmail.com>
    Date:   Thu Jul 14 18:02:09 2022 +0900
    
        Add kakao login structure
    
    commit 4c56ef1b118f3729b9c3808570327a055350f537
    Author: enebin <enebin777@gmail.com>
    Date:   Thu Jul 14 17:30:59 2022 +0900
    
        Add dependencies

---
+++ Naenio/Sources/LogIn/Kakao/KakaoLoginManager.swift
@@ -0,0 +1,32 @@
+//
+//  KakaoLogin.swift
+//  Naenio
+//
+//  Created by 이영빈 on 2022/07/14.
+//
+
+import KakaoSDKAuth
+import KakaoSDKCommon
+import KakaoSDKUser
+
+/// 카카오 서버에서 받아온 인증 정보를 네니오 서버에 보내기 위한 작업을 하는 클래스입니다
+class KakaoLoginManager {
+    func requestLoginToServer(with result: OAuthToken) -> Result<UserInformation, Error> {
+        do {
+            let token = result.accessToken
+            let loginInfo = LoginRequestInformation(accessToken: token)
+            let userInfo = try submitUserInformationToServer(with: loginInfo)
+            
+            return .success(userInfo)
+        }
+        catch let error {
+            return .failure(error)
+        }
+    }
+    
+    private func submitUserInformationToServer(with info: LoginRequestInformation) throws -> UserInformation {
+        // let userInfo = API.request(with: info)
+        let mockInfo = UserInformation(name: "", id: "")
+        return mockInfo
+    }
+}
--- Naenio/Supporting files/NaenioApp.swift
+++ Naenio/Supporting files/NaenioApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import KakaoSDKAuth
+import KakaoSDKCommon
 
 @main
 struct NaenioApp: App {
+    init() {
+        // Kakao SDK 초기화
+        KakaoSDK.initSDK(appKey: "15605dc867a5a04c1b606955957b75ce")
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().onOpenURL(perform: { url in
+                if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                    // TODO: Handle errors
+                    print("Main view", url)
+                    let _ = AuthController.handleOpenUrl(url: url)
+                }
+            })
         }
     }
 }
