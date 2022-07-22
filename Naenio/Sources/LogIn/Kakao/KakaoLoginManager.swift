02ee3c4a1256fc3c56890a35174e5726f6cc42e5
enebin <enebin777@gmail.com>
2022-07-22 22:59:57 +0900

    Change Result to Single disposable

--- Naenio/Sources/LogIn/Kakao/KakaoLoginManager.swift
+++ Naenio/Sources/LogIn/Kakao/KakaoLoginManager.swift
@@ -8,25 +8,27 @@
 import KakaoSDKAuth
 import KakaoSDKCommon
 import KakaoSDKUser
+import RxSwift
 
 /// 카카오 서버에서 받아온 인증 정보를 네니오 서버에 보내기 위한 작업을 하는 클래스입니다
 class KakaoLoginManager {
-    func requestLoginToServer(with result: OAuthToken) -> Result<UserInformation, Error> {
-        do {
-            let token = result.accessToken
-            let loginInfo = LoginRequestInformation(authToken: token, authServiceType: "KAKAO")
-            let userInfo = try submitUserInformationToServer(with: loginInfo)
-            
-            return .success(userInfo)
-        }
-        catch let error {
-            return .failure(error)
-        }
+    func requestLoginToServer(with result: OAuthToken) -> Single<UserInformation> {
+        let token = result.accessToken
+        let loginInfo = LoginRequestInformation(authToken: token, authServiceType: "KAKAO")
+        
+        return submitUserInformationToServer(with: loginInfo)
     }
     
-    private func submitUserInformationToServer(with info: LoginRequestInformation) throws -> UserInformation {
-        // let userInfo = API.request(with: info)
-        let mockInfo = UserInformation(name: "", id: "")
-        return mockInfo
+    private func submitUserInformationToServer(with info: LoginRequestInformation) -> Single<UserInformation> {
+        let sequence = NaenioAPI.login(info)
+            .request()
+            .map { response -> UserInformation in
+                let data = response.data
+                let decoded = try NaenioAPI.jsonDecoder.decode(UserInformation.self, from: data)
+                
+                return decoded
+            }
+        
+        return sequence
     }
 }
