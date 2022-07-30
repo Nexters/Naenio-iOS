8271a0af871edf0f6db023848fccc2967475c9e5
Young Bin Lee <enebin777@gmail.com>
2022-07-30 19:44:06 +0900

    Add color assets

---
+++ Naenio/Resources/Color + extension.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  Naenio
+//
+//  Created by YoungBin Lee on 2022/07/30.
+//
+
+import SwiftUI
+
+extension Color {
+    static let naenioBlue = Color("NBlue")
+    static let naenioPink = Color("NPink")
+    static let background = Color("NBackground")
+    static let card = Color("NCard")
+}
+    
