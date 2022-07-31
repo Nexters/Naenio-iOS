1e693fc9118a88d54268aa19221b3a2d3eaff5c3
Young Bin Lee <enebin777@gmail.com>
2022-07-31 13:43:53 +0900

    Add kakao-login test

--- NaenioTests/Network/LoginTests.swift
+++ NaenioTests/Network/LoginTests.swift
@@ -14,20 +14,17 @@ class LoginTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-
         provider = MoyaProvider<NaenioAPI>(stubClosure: MoyaProvider.immediatelyStub)
     }
     
     func testAppleLogin() throws {
-        let expect = expectation(description: "request")
+        let expectation = expectation(description: "request")
         
         let expected = UserInformation(token: "mock-server-token")
         var received: UserInformation?
         
         let requestInformation = LoginRequestInformation(authToken: "mock-apple-token", authServiceType: "APPLE")
         provider.request(NaenioAPI.login(requestInformation)) { result in
-            expect.fulfill()
-
             switch result {
             case .success(let response):
                 do {
@@ -39,6 +36,35 @@ class LoginTests: XCTestCase {
             case .failure(let error):
                 _ = XCTestError(_nsError: error as NSError)
             }
+            
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5)
+        XCTAssertEqual(received!.token, expected.token)
+    }
+    
+    func testKakaoLogin() throws {
+        let expectation = expectation(description: "request")
+        
+        let expected = UserInformation(token: "mock-server-token")
+        var received: UserInformation?
+        
+        let requestInformation = LoginRequestInformation(authToken: "mock-kakao-token", authServiceType: "APPLE")
+        provider.request(NaenioAPI.login(requestInformation)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let data = response.data
+                    received = try NaenioAPI.jsonDecoder.decode(UserInformation.self, from: data)
+                } catch let error {
+                    _ = XCTestError(_nsError: error as NSError)
+                }
+            case .failure(let error):
+                _ = XCTestError(_nsError: error as NSError)
+            }
+            
+            expectation.fulfill()
         }
         
         waitForExpectations(timeout: 5)
