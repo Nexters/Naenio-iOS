59750030584f1860924e4fd377cd28fd95edfef5
Young Bin Lee <enebin777@gmail.com>
2022-08-04 21:04:01 +0900

    Add categorizing

--- Naenio/Sources/Home/HomeView.swift
+++ Naenio/Sources/Home/HomeView.swift
@@ -26,44 +26,53 @@ struct HomeView: View {
                     .padding(.horizontal, 20)
                 
                 // Card scroll view
-                ScrollViewReader { proxy in
-                    ScrollView(.vertical, showsIndicators: false) {
-                        // Placeholder
-                        Rectangle()
-                            .fill(Color.clear)
-                            .frame(height: 4)
-                            .id(topID)
-                        
-                        LazyVStack(spacing: 20) {
-                            ForEach(viewModel.posts.indices, id: \.self) { index in
-                                let post = viewModel.posts[index]
-                                let cardViewModel = CardViewModel(post: post)
-                                
-                                CardView(viewModel: cardViewModel)
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 16)
-                                            .shadow(color: .black.opacity(0.5), radius: 5, x: 0, y: 0)
-                                    )
-                                    .padding(.horizontal, 20)
-                                    .redacted(reason: post.id < 0 ? .placeholder : [])
-                                    .onAppear {
-                                        if index == viewModel.posts.count - 3 {
-                                            // 무한 스크롤을 위해 끝에서 3번째에서 로딩 -> 개수는 추후 협의
-                                            #if DEBUG
-                                            print("Loaded")
-                                            #endif
-                                            viewModel.requestPosts()
+                ZStack {
+                    if viewModel.status == .inProgress {
+                        ProgressView()
+                    }
+                    
+                    ScrollViewReader { proxy in
+                        ScrollView(.vertical, showsIndicators: false) {
+                            // Placeholder
+                            Rectangle()
+                                .fill(Color.clear)
+                                .frame(height: 4)
+                                .id(topID)
+                            
+                            LazyVStack(spacing: 20) {
+                                ForEach(viewModel.posts.indices, id: \.self) { index in
+                                    let post = viewModel.posts[index]
+                                    let cardViewModel = CardViewModel(post: post)
+                                    
+                                    CardView(viewModel: cardViewModel)
+                                        .background(
+                                            RoundedRectangle(cornerRadius: 16)
+                                                .shadow(color: .black.opacity(0.5), radius: 5, x: 0, y: 0)
+                                        )
+                                        .padding(.horizontal, 20)
+    //                                    .redacted(reason: (viewModel.status == .inProgress) ? .placeholder : [])
+                                        .onAppear {
+                                            if index == viewModel.posts.count - 3 {
+                                                // 무한 스크롤을 위해 끝에서 3번째에서 로딩 -> 개수는 추후 협의
+                                                #if DEBUG
+                                                print("Loaded")
+                                                #endif
+                                                viewModel.requestMorePosts()
+                                            }
                                         }
-                                    }
+                                }
                             }
-                        }
-                        .onChange(of: viewModel.category) { _ in
-                            withAnimation(.easeOut(duration: 0.1)) {
-                                proxy.scrollTo(topID)
+                            .onChange(of: viewModel.category) { _ in
+                                viewModel.posts.removeAll()
+                                viewModel.requestPosts()
+                                withAnimation(.easeOut(duration: 0.1)) {
+                                    proxy.scrollTo(topID)
+                                }
                             }
                         }
                     }
                 }
+                
             }
             .fillScreen()
         }
--- Naenio/Sources/Home/HomeViewModel.swift
+++ Naenio/Sources/Home/HomeViewModel.swift
@@ -6,66 +6,131 @@
 //
 
 import Foundation
+import Combine
 
 class HomeViewModel: ObservableObject {
     // Published vars
     @Published var category: Category = .entire // ???: 마지막 선택 저장하는 것도 낫 배드 - using @SceneStorage
-    @Published var posts: [Post]
+    @Published var posts: [Post] // TODO: Solving data races caused by repeated network calls
     @Published var currentPage: Int = 0
+    @Published var status: Status = .waiting
+    
+    // vars and lets
+    private let pagingValue = 10
     
     // !!!: 테스트용
     @discardableResult
     func requestPosts() -> [Post] {
-        let newPosts = self.getPost()
+        status = .inProgress
+                
+        let newPosts = getPost(category: self.category)
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.posts.append(contentsOf: newPosts)
+            self.posts = newPosts
+            self.status = .done
         }
-        
+
         return newPosts
     }
     
     // !!!: 테스트용
     @discardableResult
-    func requestPosts(page: Int) -> [Post] {
-        let newPosts = self.getPost()
+    func requestMorePosts() -> [Post] {
+        status = .inProgress
+        
+        let newPosts = getPost(category: self.category)
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             self.posts.append(contentsOf: newPosts)
+            self.status = .done
         }
-        
+
         return newPosts
     }
-    
+
     init() {
         self.posts = emptyPosts
-        self.posts = self.requestPosts(page: 0)
+        self.posts = self.requestPosts()
     }
 }
 
 // !!!: 테스트용
 extension HomeViewModel {
-   func getPost() -> [Post] {
-       let posts = [Post]()
-       
-       guard let path = Bundle.main.path(forResource: "MockPostList", ofType: "json") else {
-           print("Invalid path")
-           return posts
-       }
-       
-       do {
-           let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-           let decoded = try JSONDecoder().decode([Post].self, from: data)
-           
-           return decoded
-       } catch let error {
-           print(error)
-       }
-       
-       return posts
-   }
-
+    private func getPost() -> [Post] {
+        let posts = [Post]()
+        
+        guard let path = Bundle.main.path(forResource: "MockPostList", ofType: "json") else {
+            print("Invalid path")
+            return posts
+        }
+        
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            let decoded = try JSONDecoder().decode([Post].self, from: data)
+            
+            return decoded
+        } catch let error {
+            print(error)
+        }
+        
+        return posts
+    }
+    
+    private func getPost(category: Category) -> [Post] {
+        let posts = [Post]()
+        
+        var path: String?
+        switch category {
+        case .entire:
+            path = Bundle.main.path(forResource: "MockPostList", ofType: "json")
+        case .participated:
+            path = Bundle.main.path(forResource: "MockPostParticipatedList", ofType: "json")
+        case .wrote:
+            path = Bundle.main.path(forResource: "MockPostWroteList", ofType: "json")
+        }
+        
+        guard let path = path else {
+            print("Invalid path")
+            return posts
+        }
+        
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            let decoded = try JSONDecoder().decode([Post].self, from: data)
+            
+            return decoded
+        } catch let error {
+            print(error)
+        }
+        
+        return posts
+    }
+    
 }
 
 extension HomeViewModel {
+    enum Status: Equatable {
+        static func == (lhs: HomeViewModel.Status, rhs: HomeViewModel.Status) -> Bool {
+            return lhs.description == rhs.description
+        }
+        
+        case waiting
+        case inProgress
+        case done
+        case fail(with: Error)
+        
+        var description: String {
+            switch self {
+            case .waiting:
+                return "Waiting"
+            case .inProgress:
+                return "In progres..."
+            case .done:
+                return "Successfully done"
+            case .fail(let error):
+                return "Failed with error: \(error.localizedDescription)"
+            }
+        }
+    }
+    
     enum Category: Int {
         case entire = 1
         case participated = 2
