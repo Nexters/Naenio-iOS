3a3aaaa3bf26f83d88e34bcf8ef344754ee5ee98
Young Bin Lee <enebin777@gmail.com>
2022-08-05 17:38:20 +0900

    Add Navigation

--- Naenio/Sources/Home/HomeView.swift
+++ Naenio/Sources/Home/HomeView.swift
@@ -9,71 +9,81 @@ import SwiftUI
 
 struct HomeView: View {
     @StateObject var viewModel = HomeViewModel()
+    @State var isNavigate = false
     @Namespace var topID
 
     var body: some View {
-        ZStack {
-            Color.background
-                .ignoresSafeArea()
-            
-            VStack(alignment: .leading, spacing: 20) {
-                Text("Feed")
-                    .font(.engBold(size: 24))
-                    .foregroundColor(.white)
-                    .padding(.horizontal, 20)
+        NavigationView { // FIXME: Temporary position
+            ZStack {
+                Color.background
+                    .ignoresSafeArea()
                 
-                categoryButtons
-                    .padding(.horizontal, 20)
+                navigator
                 
-                // Card scroll view
-                ZStack {
-                    if viewModel.status == .inProgress {
-                        ProgressView()
-                    }
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Feed")
+                        .font(.engBold(size: 24))
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 20)
                     
-                    ScrollViewReader { proxy in
-                        ScrollView(.vertical, showsIndicators: false) {
-                            // Placeholder
-                            Rectangle()
-                                .fill(Color.clear)
-                                .frame(height: 4)
-                                .id(topID)
-                            
-                            LazyVStack(spacing: 20) {
-                                ForEach(Array(viewModel.posts.enumerated()), id: \.element.id) { (index: Int, post: Post) in
-                                    let cardViewModel = CardViewModel(post: post)
-                                    
-                                    CardView(viewModel: cardViewModel)
-                                        .background(
-                                            RoundedRectangle(cornerRadius: 16)
-                                                .shadow(color: .black.opacity(0.5), radius: 5, x: 0, y: 0)
-                                        )
-                                        .padding(.horizontal, 20)
-    //                                    .redacted(reason: (viewModel.status == .inProgress) ? .placeholder : [])
-                                        .onAppear {
-                                            if index == viewModel.posts.count - 3 {
-                                                // 무한 스크롤을 위해 끝에서 3번째에서 로딩 -> 개수는 추후 협의
-                                                #if DEBUG
-                                                print("Loaded")
-                                                #endif
-                                                viewModel.requestMorePosts()
+                    categoryButtons
+                        .padding(.horizontal, 20)
+                    
+                    // Card scroll view
+                    ZStack {
+                        if viewModel.status == .inProgress {
+                            ProgressView()
+                        }
+                        
+                        ScrollViewReader { proxy in
+                            ScrollView(.vertical, showsIndicators: false) {
+                                // Placeholder
+                                Rectangle()
+                                    .fill(Color.clear)
+                                    .frame(height: 4)
+                                    .id(topID)
+                                
+                                LazyVStack(spacing: 20) {
+                                    ForEach(Array(viewModel.posts.enumerated()), id: \.element.id) { (index: Int, post: Post) in
+                                        let cardViewModel = CardViewModel(post: post)
+
+                                        CardView(viewModel: cardViewModel)
+                                            .background(
+                                                RoundedRectangle(cornerRadius: 16)
+                                                    .shadow(color: .black.opacity(0.5), radius: 5, x: 0, y: 0)
+                                            )
+                                            .padding(.horizontal, 20)
+        //                                    .redacted(reason: (viewModel.status == .inProgress) ? .placeholder : [])
+                                            .onTapGesture {
+                                                print("Tapped")
+                                                isNavigate = true
                                             }
-                                        }
+                                            .onAppear {
+                                                if index == viewModel.posts.count - 3 {
+                                                    // 무한 스크롤을 위해 끝에서 3번째에서 로딩 -> 개수는 추후 협의
+                                                    #if DEBUG
+                                                    print("Loaded")
+                                                    #endif
+                                                    viewModel.requestMorePosts()
+                                                }
+                                            }
+                                    }
                                 }
-                            }
-                            .onChange(of: viewModel.category) { _ in
-                                viewModel.posts.removeAll()
-                                viewModel.requestPosts()
-                                withAnimation(.easeOut(duration: 0.1)) {
-                                    proxy.scrollTo(topID)
+                                .onChange(of: viewModel.category) { _ in
+                                    viewModel.posts.removeAll()
+                                    viewModel.requestPosts()
+                                    withAnimation(.easeOut(duration: 0.1)) {
+                                        proxy.scrollTo(topID)
+                                    }
                                 }
                             }
                         }
                     }
+                    
                 }
-                
+                .fillScreen()
             }
-            .fillScreen()
+            .navigationBarHidden(true)
         }
     }
 }
@@ -113,6 +123,12 @@ extension HomeView {
                 Capsule()
                     .shadow(color: .black.opacity(0.5), radius: 5, x: 0, y: 0)
             )
+        }
+    }
+    
+    var navigator: some View {
+        NavigationLink(destination: Text("Destination"), isActive: $isNavigate) {
+            EmptyView()
         }
     }
 }
