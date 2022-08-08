dbfeeed9260abb5098bc312b4983387c38449ccd
Young Bin Lee <enebin777@gmail.com>
2022-08-08 20:28:54 +0900

    Lint codes Line codes Lint codes

--- Naenio/Sources/Home/Vote/VotesView.swift
+++ Naenio/Sources/Home/Vote/VotesView.swift
@@ -21,22 +21,18 @@ struct VotesView: View {
 
     var body: some View {
         ZStack {
-            // TODO: 추가적인 작업 필요함. 지금은 액션 구현만 되어있는데 정확한 동작은 따로 브랜치 만들어서 작업할 예정
-            // (일단 싱크 위해서 머지만 해놓고)
-            VStack(spacing: 18) { // ???: 어느 미래에 선택지가 2개가 아닌 1개만 들어오는 케이스를 대비할 필요도 있음
+            VStack(spacing: 18) {
                 VoteButton(type: .choiceA, isOpened: self.isOpened, choice: choices.first) {
                     withAnimation(.easeInOut(duration: 0.2)) {
                         sourceObject.vote(index: self.index, sequence: 0)
                     }
                 }
-//                .frame(height: 72)
                 
                 VoteButton(type: .choiceB, isOpened: self.isOpened, choice: choices.last) {
                     withAnimation(.easeInOut(duration: 0.2)) {
                         sourceObject.vote(index: self.index, sequence: 1)
                     }
                 }
-//                .frame(height: 72)
             }
             
             Text("VS")
