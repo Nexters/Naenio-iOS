d3f42ac460f0fe177921bc2d779eef4e058e232b
Young Bin Lee <enebin777@gmail.com>
2022-08-12 10:31:34 +0900

    [Fix] Disable dismissing keyboard when TextView is tapped

--- Naenio/Sources/Home/NewPost/NewPostView.swift
+++ Naenio/Sources/Home/NewPost/NewPostView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import Combine
 
 struct NewPostView: View {
     @EnvironmentObject var sourceObject: HomeViewModel
@@ -18,66 +17,69 @@ struct NewPostView: View {
     @State var details: String = ""
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            ZStack(alignment: .center) {
-                closeAndRegisterButtons
-
-                Text("New Post")
-                    .font(.engBold(size: 18))
-                    .foregroundColor(.white)
-            }
-            .padding(.bottom, 32)
-            
-            // MARK: - 투표 주제
-            Text("*투표 주제")
-                .foregroundColor(.white)
-                .font(.medium(size: 16))
-                .id(1)
-            
-            TextView(placeholder: "무슨 주제를 담아볼까요?", content: $title, characterLimit: 72)
-                .frame(height: 108)
-                .padding(.bottom, 20)
-            
-            // MARK: - 투표 선택지
-            Text("*투표 선택지")
-                .foregroundColor(.white)
-                .font(.medium(size: 16))
-            
-            ZStack(alignment: .center) {
-                Image("vsIcon")
-                    .resizable()
-                    .frame(width: 36, height: 36)
-                    .scaledToFit()
-                    .zIndex(1)
-                
-                VStack(spacing: 20) {
-                    TextView(placeholder: "A의 선택지를 입력해 주세요", content: $choiceA, characterLimit: 32)
-                        .frame(height: 70)
-                    
-                    TextView(placeholder: "B의 선택지를 입력해 주세요", content: $choiceB, characterLimit: 32)
-                        .frame(height: 70)
+        ZStack {
+            Color.background
+                .ignoresSafeArea()
+                .onTapGesture {
+                    // Take focus out of text view when tapped
+                    UIApplication.shared.endEditing()
                 }
+            
+            VStack(alignment: .leading, spacing: 4) {
+                ZStack(alignment: .center) {
+                    closeAndRegisterButtons
+
+                    Text("New Post")
+                        .font(.engBold(size: 18))
+                        .foregroundColor(.white)
+                }
+                .padding(.bottom, 32)
+                
+                // MARK: - 투표 주제
+                Text("*투표 주제")
+                    .foregroundColor(.white)
+                    .font(.medium(size: 16))
+                
+                TextView(placeholder: "무슨 주제를 담아볼까요?", content: $title, characterLimit: 72)
+                    .frame(height: 108)
+                    .padding(.bottom, 20)
+                
+                // MARK: - 투표 선택지
+                Text("*투표 선택지")
+                    .foregroundColor(.white)
+                    .font(.medium(size: 16))
+                
+                ZStack(alignment: .center) {
+                    Image("vsIcon")
+                        .resizable()
+                        .frame(width: 36, height: 36)
+                        .scaledToFit()
+                        .zIndex(1)
+                    
+                    VStack(spacing: 20) {
+                        TextView(placeholder: "A의 선택지를 입력해 주세요", content: $choiceA, characterLimit: 32)
+                            .frame(height: 70)
+                        
+                        TextView(placeholder: "B의 선택지를 입력해 주세요", content: $choiceB, characterLimit: 32)
+                            .frame(height: 70)
+                    }
+                }
+                .padding(.bottom, 20)
+                
+                // MARK: - 내용
+                Text("내용")
+                    .foregroundColor(.white)
+                    .font(.medium(size: 16))
+                
+                TextView(placeholder: "어떤 내용을 추가로 담을까요?", content: $details, characterLimit: 100)
+                    .frame(height: 108)
+                    .padding(.bottom, 200)
             }
-            .padding(.bottom, 20)
-            
-            // MARK: - 내용
-            Text("내용")
-                .foregroundColor(.white)
-                .font(.medium(size: 16))
-            
-            TextView(placeholder: "어떤 내용을 추가로 담을까요?", content: $details, characterLimit: 100)
-                .frame(height: 108)
-                .padding(.bottom, 200)
+            .keyboardAdaptive()
+            .padding(.top, 24)
+            .padding(.horizontal, 24)
         }
-        .keyboardAdaptive()
-        .padding(.top, 24)
-        .padding(.horizontal, 24)
         .fillScreen()
-        .background(Color.background.ignoresSafeArea())
-        .onTapGesture {
-            // Take focus out of text view when tapped
-            UIApplication.shared.endEditing()
-        }
     }
     
     func makePostRequest(title: String, details: String, choiceA: String, choiceB: String) -> PostRequestInformation {
@@ -100,8 +102,9 @@ extension NewPostView {
             Button(action: { isPresented = false }) {
                 Image(systemName: "xmark")
                     .resizable()
-                    .frame(width: 16, height: 16)
                     .scaledToFit()
+                    .frame(width: 16, height: 16)
+                    .padding([.top, .bottom, .trailing], 6)
                     .foregroundColor(.white)
             }
             
