b9bccd0f80437836376c62b6ce7da0dc0f50ff32
Choyoonyoung98 <yoon980208@gmail.com>
2022-08-13 21:56:57 +0900

    [UPDATE] 로그인 및 앱 진입 시점에서 유저 데이터 초기화
    - // FIXME: 회원가입 로직이 구현되지 않아 우선 nickname = "user", profileImageIndex = 0으로 임시 초기화

--- Naenio/Sources/LogIn/Kakao/KakaoLoginManager.swift
+++ Naenio/Sources/LogIn/Kakao/KakaoLoginManager.swift
@@ -13,15 +13,14 @@ import RxSwift
 /// 카카오 서버에서 받아온 인증 정보를 네니오 서버에 보내기 위한 작업을 하는 클래스입니다
 class KakaoLoginManager {
     let loginRequestService: LoginRequestService
-    
     init(_ loginRequestService: LoginRequestService = LoginRequestService()) {
         self.loginRequestService = loginRequestService
     }
     
     func requestLoginToServer(with result: OAuthToken) -> Single<UserInformation> {
         let token = result.accessToken
-        let loginInfo = LoginRequestInformation(authToken: token, authServiceType: "KAKAO")
-        
+        let loginInfo = LoginRequestInformation(authToken: token, authServiceType: AuthServiceType.kakao.rawValue)
+        UserManager.shared.updateAuthServiceType(AuthServiceType.kakao.rawValue)
         return loginRequestService.submitUserInformationToServer(with: loginInfo)
     }
 }
