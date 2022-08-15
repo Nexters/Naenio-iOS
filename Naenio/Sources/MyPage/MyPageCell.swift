1bc61a5cfa4e6e42af06262ea2a5f3586c38bd57
Young Bin Lee <enebin777@gmail.com>
2022-08-15 16:58:00 +0900

    [Feature] Set my page layout

---
+++ Naenio/Sources/MyPage/MyPageCell.swift
@@ -0,0 +1,33 @@
+//
+//  MyPageCell.swift
+//  Naenio
+//
+//  Created by YoungBin Lee on 2022/08/15.
+//
+
+import SwiftUI
+
+struct MyPageCell<Leading, Trailing>: View where Leading: View, Trailing: View {
+    let leadingView: Leading
+    let trailingView: Trailing
+    
+    var body: some View {
+        HStack {
+            leadingView
+            
+            Spacer()
+            
+            trailingView
+        }
+        .padding(.horizontal, 24)
+        .frame(height: 57)
+        .fillHorizontal()
+        .background(Color.card)
+        .foregroundColor(.white)
+    }
+    
+    init(@ViewBuilder leading: () -> Leading, @ViewBuilder trailing: () -> Trailing) {
+        self.leadingView = leading()
+        self.trailingView = trailing()
+    }
+}
---
+++ Naenio/Sources/MyPage/MyPageSection.swift
@@ -0,0 +1,23 @@
+//
+//  MyPageForEach.swift
+//  Naenio
+//
+//  Created by YoungBin Lee on 2022/08/15.
+//
+
+import SwiftUI
+
+struct MyPageSection<Content>: View where Content: View {
+    let content: Content
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            content
+        }
+        .cornerRadius(10)
+    }
+    
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content()
+    }
+}
