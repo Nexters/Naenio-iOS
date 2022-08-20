dbcdf0aef54ddaf90aca367d985cfee3d082f12d
Choyoonyoung98 <yoon980208@gmail.com>
2022-08-20 12:22:15 +0900

    [UPDATE] splash image asset 추가

---
+++ Naenio/Utils/Network/Constants/KeyValue.swift
@@ -0,0 +1,13 @@
+//
+//  KeyValue.swift
+//  Naenio
+//
+//  Created by 이영빈 on 2022/07/21.
+//
+
+import Foundation
+
+struct KeyValue {
+    static let baseURL = "https://teamversus.shop"
+    static let kakaoAPIkey = "15605dc867a5a04c1b606955957b75ce"
+}
