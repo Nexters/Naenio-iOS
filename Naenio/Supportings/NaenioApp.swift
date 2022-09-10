fadb5cb2e36cc3a257e7c2266ea52d2a02df8870
enebin <enebin777@gmail.com>
2022-09-10 19:24:51 +0900

    [Fix] Hide navigation header

--- Naenio/Supportings/NaenioApp.swift
+++ Naenio/Supportings/NaenioApp.swift
@@ -63,6 +63,8 @@ struct NaenioApp: App {
                                                EmptyView()
                                            }
                         )
+                        .navigationBarHidden(true)
+                        .navigationBarTitle("", displayMode: .inline)
                 }
             } else {
                 ZStack(alignment: .center) {
