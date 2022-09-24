fa5365e592eb58dc2f04ab35a230642e0ed19ca4
enebin <enebin777@gmail.com>
2022-09-24 12:42:07 +0900

    [Feature] Ad 400 test

--- NaenioTests/Network/NetworkResponseHandlingTests.swift
+++ NaenioTests/Network/NetworkResponseHandlingTests.swift
@@ -6,30 +6,50 @@
 //
 
 import XCTest
+import Moya
+import Foundation
+import RxSwift
+
+@testable import Naenio
 
 final class NetworkResponseHandlingTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    let bag = DisposeBag()
+    
+    func getErrorProvider(giving statusCode: Int) -> MoyaProvider<NaenioAPI> {
+        let endpointClosure = { (target: NaenioAPI) -> Endpoint in
+            return Endpoint(url: URL(target: target).absoluteString,
+                            sampleResponseClosure: { .networkResponse(statusCode, Data()) },
+                            method: target.method,
+                            task: target.task,
+                            httpHeaderFields: target.headers)
         }
+
+        let stubbingProvider = MoyaProvider<NaenioAPI>(endpointClosure: endpointClosure, stubClosure: MoyaProvider.immediatelyStub)
+        
+        return stubbingProvider
     }
 
+    func test_handlingResponse_400() throws {
+        let provider = getErrorProvider(giving: 400)
+        let expectation = expectation(description: "request")
+        
+        NaenioAPI.deleteAccount.request(provider: provider)
+            .subscribe(
+                onSuccess: { response in
+                    XCTFail("Wrong status code is not handled properly: \(response.statusCode)")
+                },
+                onFailure: { error in
+                    print(error)
+                    _ = XCTestError(_nsError: error as NSError)
+                },
+                onDisposed: {
+                    expectation.fulfill()
+                }
+            )
+            .disposed(by: bag)
+            
+        
+        waitForExpectations(timeout: 5)
+//        XCTAssertEqual(received!.token, expected.token)
+    }
 }
